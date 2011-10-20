2ba69e7051913163fbcc2015530a012f805f9d79
Truong Hoang Dung <dungth@hpu.edu.vn>
2011-10-20 16:01:20 +0700

    Added template

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,11 @@ import java.util.*;
 import models.*;
 
 public class Application extends Controller {
-
+	@Before
+	static void addDefaults() {
+	    renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+	    renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+	}
     public static void index() {
     	// get first post
     	Post frontPost = Post.find("order by postedAt desc").first();    	    
@@ -17,5 +21,8 @@ public class Application extends Controller {
     	// render both
         render(frontPost, olderPosts);
     }
-
+    public static void show(Long id) {
+    	Post post = Post.findById(id);
+    	render(post);
+    }
 }
