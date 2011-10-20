315e5a820a589154cb7801af07bb7b25f4e8af19
Truong Hoang Dung <dungth@hpu.edu.vn>
2011-10-20 08:34:10 +0700

    init

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -4,10 +4,26 @@ import play.test.*;
 import models.*;
 
 public class BasicTest extends UnitTest {
-
+   
     @Test
-    public void aVeryImportantThingToTest() {
-        assertEquals(2, 1 + 1);
+    public void createAndRetrieveUser()
+    {
+    	new User("dungth@hpu.edu.vn", "secret","Rev").save();
+    	// retrieve
+    	User rev = User.find("byEmail", "dungth@hpu.edu.vn").first();
+    	assertNotNull(rev);
+    	assertEquals("Rev", rev.fullname);
+    	
     }
-
+    @Test
+    public void tryConnectAsUser() {
+        // Create a new user and save it
+        new User("bob@gmail.com", "secret", "Bob").save();
+        
+        // Test 
+        assertNotNull(User.connect("bob@gmail.com", "secret"));
+        assertNull(User.connect("bob@gmail.com", "badpassword"));
+        assertNull(User.connect("tom@gmail.com", "secret"));
+    }
+    
 }
