e33b099ba732e41ab0ab879f597666af62ffaca7
Truong Hoang Dung <dungth@hpu.edu.vn>
2011-10-20 15:21:26 +0700

    Added bootstrap

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -13,9 +13,9 @@ public class Application extends Controller {
     	// get first post
     	Post frontPost = Post.find("order by postedAt desc").first();    	    
     	// find 10 latest posts desc
-    	List<Post> posts = Post.find("order by postedAt desc").from(1).fetch(10);
+    	List<Post> olderPosts = Post.find("order by postedAt desc").from(1).fetch(10);
     	// render both
-        render(frontPost, posts);
+        render(frontPost, olderPosts);
     }
 
 }
