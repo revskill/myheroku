34f7e5043cd6c4ee3086ec3bc5b93a3ee097e657
Truong Hoang Dung <dungth@hpu.edu.vn>
2011-10-21 15:19:22 +0700

    Tempo

--- app/models/Post.java
+++ app/models/Post.java
@@ -5,10 +5,13 @@ package models;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
@@ -25,8 +28,13 @@ public class Post extends Model{
 	
 	@OneToMany(mappedBy="post", cascade=CascadeType.ALL)
 	public List<Comment> comments;
+	
+	@ManyToMany(cascade=CascadeType.PERSIST)
+	public Set<Tag> tags;
+	
 	public Post(User author, String title, String content) {
 		this.comments = new ArrayList<Comment>();
+		this.tags = new TreeSet<Tag>();
 		this.author = author;
 		this.title = title;
 		this.postedAt = new Date();
@@ -45,4 +53,21 @@ public class Post extends Model{
 	public Post next() {
 	    return Post.find("postedAt > ? order by postedAt asc", postedAt).first();
 	}
+	public Post tagItWith(String name) {
+	    tags.add(Tag.findOrCreateByName(name));
+	    return this;
+	}
+	
+	public static List<Post> findTaggedWith(String tag) {
+	    return Post.find(
+	        "select distinct p from Post p join p.tags as t where t.name = ?", tag
+	    ).fetch();
+	}
+	public static List<Post> findTaggedWith(String... tags) {
+	    return Post.find(
+	        "select distinct p.id from Post p join p.tags as t where t.name in (:tags) group by p.id having count(t.id) = :size"
+	    ).bind("tags", tags).bind("size", tags.length).fetch();
+	}
+	
+	
 }
--- test/BasicTest.java
+++ test/BasicTest.java
@@ -140,4 +140,26 @@ public class BasicTest extends UnitTest {
         assertEquals(3, frontPost.comments.size());
         assertEquals(4, Comment.count());
     }
+    @Test
+    public void testTags() {
+        // Create a new user and save it
+        User bob = new User("bob@gmail.com", "secret", "Bob").save();
+     
+        // Create a new post
+        Post bobPost = new Post(bob, "My first post", "Hello world").save();
+        Post anotherBobPost = new Post(bob, "Hop", "Hello world").save();
+        
+        // Well
+        assertEquals(0, Post.findTaggedWith("Red").size());
+        
+        // Tag it now
+        bobPost.tagItWith("Red").tagItWith("Blue").save();
+        anotherBobPost.tagItWith("Red").tagItWith("Green").save();
+        
+        // Check
+        assertEquals(2, Post.findTaggedWith("Red").size());        
+        assertEquals(1, Post.findTaggedWith("Blue").size());
+        assertEquals(1, Post.findTaggedWith("Green").size());
+        
+    }
 }
